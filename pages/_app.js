9f4765af08bcb798920a3069adc7b46bfe740674
parth-sumo <parth.langalia.ctr@sumologic.com>
2025-04-29 05:28:02 +0530

    Updated paths in app.js and document.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,34 +7,30 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider>
       <Head>
         {/* Favicon (CSR) */}
-        <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicons/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/mrpal/favicons/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/mrpal/favicons/favicon.ico" type="image/x-icon" />
 
-        {/* Apple Touch Icons - Multiple sizes for different devices */}
-        <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#111827" />
+        {/* Apple Touch Icons */}
+        <link rel="apple-touch-icon" sizes="57x57" href="/mrpal/favicons/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/mrpal/favicons/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/mrpal/favicons/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/mrpal/favicons/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/mrpal/favicons/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/mrpal/favicons/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/mrpal/favicons/apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/mrpal/favicons/apple-touch-icon.png" />
+        <link rel="mask-icon" href="/mrpal/favicons/safari-pinned-tab.svg" color="#111827" />
 
-        {/* Safari Pinned Tab Icon */}
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
-        {/* Viewport */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
           key="viewport"
         />
 
-        {/* Resource Hints */}
         <link
           rel="preload"
-          href="/favicons/favicon.svg"
+          href="/mrpal/favicons/favicon.svg"
           as="image"
           type="image/svg+xml"
           key="preload-favicon"
